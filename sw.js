bf2241372e91d64a69c475f0e22aae25374d14eb
koperasiRAS <info.koperasirayaabadisaudara@gmail.com>
2026-01-19 02:30:34 +0700

    Update sw.js

---
+++ sw.js
@@ -0,0 +1,42 @@
+// Service Worker untuk caching
+const CACHE_NAME = 'inventory-pro-v2.0';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/react@18/umd/react.development.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.development.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js',
+  'https://fonts.googleapis.com/css2?family=Libre+Barcode+128&display=swap'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch from cache first
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+// Clean up old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
